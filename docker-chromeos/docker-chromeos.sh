d36dfa229b41b79add27fb251085664a5997f1bc
Micah Catlin <micah@looker.com>
2018-12-05 23:51:22 +0000

    Add Docker repository for docker-ce

--- docker-chromeos/docker-chromeos.sh
+++ docker-chromeos/docker-chromeos.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   stretch \
+   stable"
+
+sudo apt-get update
 sudo apt-get install docker-ce
 sudo cp runc-chromeos /usr/local/bin
 sudo cp daemon.json /etc/docker
